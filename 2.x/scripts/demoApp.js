e4aec454ebe7697dec9c970f34a9bdba4cb697ac
Ryan A. Johnson <ryan.johnson@rackspace.com>
2017-02-21 13:42:47 -0600

    docs(ghpages): release v2.3.6 [skip ci]

--- 2.x/scripts/demoApp.js
+++ 2.x/scripts/demoApp.js
@@ -214,7 +214,7 @@ angular.module('demoApp', ['encore.ui', 'ngRoute'])
             type: 'no-title',
             children: [
                 {
-                    linkText: 'Version 2.3.6-0',
+                    linkText: 'Version 2.3.6',
                     directive: 'switch-docs',
                     children: [{}],
                     childVisibility: 'false'
