e3e2bbb8d71c1f6ec7cf9b8f967009458cd2d66a
jj4th <john.andrichak@gmail.com>
2017-03-15 16:05:53 -0500

    chore(rxPageObjects): rxFeedback to TypeScript

---
+++ utils/rx-page-objects/src/rxFeedback.page.ts
@@ -0,0 +1,119 @@
+'use strict';
+
+import {ElementFinder} from 'protractor';
+import {$, browser, by} from 'protractor';
+import {AccessorPromiseString} from './rxComponent';
+
+import {rxModalAction} from './rxModalAction.page';
+import {rxNotify} from './rxNotify.page';
+import {rxSelect} from './rxSelect.page';
+
+/**
+ * @namespace
+ * @description Utilities for interacting with an rxFeedback component.
+ */
+export class rxFeedback extends rxModalAction {
+    lnkFeedback: ElementFinder;
+
+    // Since we're technically an rxModal, we need to change the root element.
+    constructor(readonly rootElement: ElementFinder) {
+        super($('.modal'));
+        this.lnkFeedback = rootElement;
+    }
+
+    get selReportType() {
+        return new rxSelect($('#selFeedbackType'));
+    }
+
+    get txtFeedback() {
+        return this.element(by.model('fields.description'));
+    }
+
+    /**
+     * @description Opens the feedback modal.
+     */
+    open() {
+        return this.isDisplayed().then(isDisplayed => {
+            if (!isDisplayed) {
+                this.lnkFeedback.$('a').click();
+            }
+        });
+    }
+
+    /**
+     * @description A getter and setter for changing the type of feedback to be submitted.
+     * @example
+     * feedback = new rxFeedback();
+     * feedback.open();
+     * feedback.type = 'Kudos';
+     * expect(feedback.type).to.eventually.equal('Kudos');
+     */
+    get type(): AccessorPromiseString {
+        return this.selReportType.selectedOption.getText();
+    }
+    set type(optionText) {
+        this.selReportType.select(optionText);
+    }
+
+    /**
+     * @description All feedback types available for submission.
+     */
+    getTypes() {
+        return this.selReportType.options.getText();
+    }
+
+    /**
+     * @description A getter and setter to get or change the feedback's description text.
+     */
+    get description(): AccessorPromiseString {
+        return this.txtFeedback.getAttribute('value');
+    }
+    set description(feedback) {
+        this.txtFeedback.clear();
+        this.txtFeedback.sendKeys(feedback);
+    }
+
+    /**
+     * @description The placeholder string that populates the feedback description by default.
+     */
+    getDescriptionPlaceholder() {
+        return this.txtFeedback.getAttribute('placeholder');
+    }
+
+    /**
+     * @description The label above the description text box.
+     */
+    getDescriptionLabel() {
+        return this.$('.feedback-description').getText();
+    }
+
+    /**
+     * @description A high-level utility function for quickly submitting feedback.
+     * Prepares, writes, and submits feedback.
+     * If `confirmSuccessWithin` is defined, a confirmation of submission success must appear
+     * within `confirmSuccessWithin` milliseconds.
+     * If `confirmSuccessFn` is undefined, the default behavior will look for an rxNotify success
+     * message. Otherwise, `confirmSuccessFn` will be attempted until it yields a truthy value,
+     * using Protractor's `wait` function.  You must also specify a value for `confirmSuccessWithin`.
+     */
+    send(feedbackType: string, feedbackText: string, confirmSuccessWithin?: number, confirmSuccessFn?: Function) {
+        this.open();
+        this.type = feedbackType;
+        this.description = feedbackText;
+        if (confirmSuccessWithin !== undefined) {
+            this.submit();
+            return this.confirmSuccess(confirmSuccessWithin, confirmSuccessFn);
+        }
+        return this.submit();
+    }
+
+    /**
+     * @description Helper function used to confirm that {@link rxFeedback#send} was confirmed as successful.
+     * @see rxFeedback#send
+     */
+    confirmSuccess(within: number, fn?: Function) {
+        fn = fn || rxNotify.all.isPresent.bind(rxNotify.all, 'feedback', 'success');
+
+        return browser.wait(fn, within, 'Feedback submission did not confirm success within ' + within + ' msecs');
+    }
+}// rxFeedback
---
+++ utils/rx-page-objects/test/rxFeedback.midway.ts
@@ -0,0 +1,94 @@
+'use strict';
+
+import {expect} from 'chai';
+import * as _ from 'lodash';
+import {$, browser} from 'protractor';
+
+import {rxFeedback} from '../index';
+
+let demoPage = require('../../demo.page');
+
+describe('rxFeedback', () => {
+    let successfulFeedback: rxFeedback;
+    let unsuccessfulFeedback: rxFeedback;
+    let defaultFeedback = 'Software Bug';
+
+    before(() => {
+        demoPage.go('#/elements/Feedback');
+        successfulFeedback = new rxFeedback($('#rxFeedbackSucceeds'));
+        unsuccessfulFeedback = new rxFeedback($('#rxFeedbackFails'));
+    });
+
+    it('should select the "' + defaultFeedback + '" feedback type by default', () => {
+        successfulFeedback.open();
+        expect(successfulFeedback.type).to.eventually.equal(defaultFeedback);
+    });
+
+    it('should have the default feedback description label for "' + defaultFeedback + '"', () => {
+        expect(successfulFeedback.getDescriptionLabel()).to.eventually.equal('Bug Description:');
+    });
+
+    it('should have the default feedback placeholder text for "' + defaultFeedback + '"', () => {
+        let placeholder = 'Please be as descriptive as possible so we can track it down for you.';
+        expect(successfulFeedback.getDescriptionPlaceholder()).to.eventually.equal(placeholder);
+    });
+
+    describe('feedback types and labels', () => {
+        let typesAndLabels = {
+            'Incorrect Data': {
+                descriptionLabel: 'Problem Description:',
+                descriptionPlaceholder: ['Please be as descriptive as possible ',
+                                         'so we can figure it out for you.'].join(''),
+            },
+            'Feature Request': {
+                descriptionLabel: 'Feature Description:',
+                descriptionPlaceholder: ['Please be as descriptive as possible ',
+                                         'so we can make your feature awesome.'].join(''),
+            },
+            'Kudos': {
+                descriptionLabel: 'What made you happy?:',
+                descriptionPlaceholder: ['We love to hear that you\'re enjoying Encore! ',
+                                         'Tell us what you like, and what we can do to ',
+                                         'make it even better'].join(''),
+            },
+        };
+
+        it('should have all feedback types', () => {
+            let types = [defaultFeedback].concat(_.keys(typesAndLabels));
+            expect(successfulFeedback.getTypes()).to.eventually.eql(types);
+        });
+
+        _.forEach(typesAndLabels, (typeData, type) => {
+            it('should switch feedback types', () => {
+                successfulFeedback.type = type;
+                expect(successfulFeedback.type).to.eventually.equal(type);
+            });
+
+            it('should have the correct label set for description', () => {
+                expect(successfulFeedback.getDescriptionLabel())
+                    .to.eventually.equal(typeData.descriptionLabel);
+            });
+
+            it('should have the correct description placeholder', () => {
+                expect(successfulFeedback.getDescriptionPlaceholder())
+                    .to.eventually.equal(typeData.descriptionPlaceholder);
+            });
+
+        });
+
+    });
+
+    describe('submitting feedback', () => {
+
+        it('should successfully submit feedback', () => {
+            expect(successfulFeedback.send('Software Bug', 'test', 3000)).to.not.be.rejectedWith(Error);
+        });
+
+        it('should catch errors on unsuccessful feedback', () => {
+            browser.sleep(3000);
+            expect(unsuccessfulFeedback.send('Software Bug', 'test', 3000)).to.be.rejectedWith(Error);
+        });
+
+    });
+
+});
