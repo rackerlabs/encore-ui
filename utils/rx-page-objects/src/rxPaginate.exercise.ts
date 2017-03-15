0bc84a7f1a8672073e9fb971b97f5dfa4b563046
jj4th <john.andrichak@gmail.com>
2017-03-15 16:05:54 -0500

    chore(rxPageObjects): rxPaginate to TypeScript

---
+++ utils/rx-page-objects/src/rxPaginate.exercise.ts
@@ -0,0 +1,177 @@
+'use strict';
+
+import {expect} from 'chai';
+import * as _ from 'lodash';
+
+import {rxMisc} from './rxMisc.page';
+import * as component from './rxPaginate.page';
+
+interface IRxPaginateExerciseOptions {
+    instance: component.rxPaginate;
+    present?: boolean;
+    displayed?: boolean;
+    pages?: number;
+    pageSizes?: number[];
+    totalItems?: number;
+    valid?: boolean;
+    defaultPageSize?: number;
+}
+/**
+ * rxPaginate exercises.
+ * @example
+ * describe('default exercises', encore.exercise.rxPaginate({
+ *     instance: myPage.pagination, // select one of many pagination instances from your page objects
+ *     pages: 20 // will exercise full functionality at 6, limited functionality at 2
+ *     totalItems: 300 // some tests require the total number of items to be specified in advance.
+ * }));
+ */
+export function rxPaginate (options: IRxPaginateExerciseOptions) {
+    options = _.defaults(options, {
+        present: true,
+        displayed: true,
+        pages: 6,
+        pageSizes: [50, 200, 350, 500],
+        defaultPageSize: 50,
+        totalItems: 251,
+    });
+
+    return () => {
+        let pagination: component.rxPaginate;
+
+        if (!options.present) {
+            return;
+        }
+
+        before(() => {
+            pagination = options.instance;
+        });
+
+        it(`should ${options.displayed ? 'be' : 'not be'} displayed`, () => {
+            expect(pagination.isDisplayed()).to.eventually.equal(options.displayed);
+        });
+
+        if (!options.displayed) {
+            return;
+        }
+
+        it('should not have `next` link on the last page', () => {
+            pagination.last();
+            expect(pagination.lnkNext.isDisplayed()).to.eventually.be.false;
+        });
+
+        it('should allow attempting to navigate to the next page when already on the last page', () => {
+            pagination.last();
+            pagination.getPage().then(page => {
+                pagination.next();
+                expect(pagination.getPage()).to.eventually.equal(page);
+            });
+        });
+        it('should allow attempting to navigate to the last page when already on the last page', () => {
+            pagination.last();
+            pagination.getPage().then(page => {
+                pagination.last();
+                expect(pagination.getPage()).to.eventually.equal(page);
+            });
+        });
+
+        it('should navigate to the first page', () => {
+            pagination.first();
+            expect(pagination.getPage()).to.eventually.equal(1);
+        });
+
+        it('should not have `prev` link on the first page', () => {
+            pagination.first();
+            expect(pagination.lnkPrev.isDisplayed()).to.eventually.be.false;
+        });
+
+        it('should allow attempting to navigate to the previous page when already on the first page', () => {
+            pagination.first();
+            pagination.previous();
+            expect(pagination.getPage()).to.eventually.equal(1);
+        });
+
+        it('should allow attempting to navigate to the first page when already on the first page', () => {
+            pagination.first();
+            expect(pagination.getPage()).to.eventually.equal(1);
+        });
+
+        it('should have all available page sizes', () => {
+            expect(pagination.getPageSizes()).to.eventually.eql(options.pageSizes);
+        });
+
+        it('should highlight the current items per page selection', () => {
+            expect(pagination.getPageSize()).to.eventually.equal(options.defaultPageSize);
+        });
+
+        if (options.pages > 1) {
+            it('should navigate forward one page at a time', () => {
+                pagination.next();
+                expect(pagination.getPage()).to.eventually.equal(2);
+            });
+
+            it('should navigate backwards one page at a time', () => {
+                pagination.previous();
+                expect(pagination.getPage()).to.eventually.equal(1);
+            });
+
+            it('should navigate to the last page', () => {
+                pagination.getPage().then(page => {
+                    let firstPage = page;
+                    pagination.last();
+                    rxMisc.scrollToElement(pagination, {
+                        positionOnScreen: 'middle',
+                    });
+                    expect(pagination.getPage()).to.eventually.be.above(firstPage);
+                    pagination.first();
+                });
+            });
+        }
+
+        if (options.pages > 5) {
+            it('should jump forward to page 6 using pagination', () => {
+                pagination.jumpToPage(6);
+                expect(pagination.getPage()).to.eventually.equal(6);
+            });
+
+            it('should jump backward to page 2 using pagination', () => {
+                pagination.jumpToPage(2);
+                expect(pagination.getPage()).to.eventually.equal(2);
+                pagination.jumpToPage(1);
+            });
+        }
+
+        if (options.pageSizes.length > 1) {
+
+            it('should switch to a different items per page', () => {
+                pagination.changePageSize(options.pageSizes[1]);
+                expect(pagination.getPageSize()).to.eventually.equal(options.pageSizes[1]);
+                pagination.changePageSize(options.pageSizes[0]);
+            });
+
+            if (options.pages > 1) {
+                it('should put the user back on the first page after resizing the items per page', () => {
+                    pagination.jumpToPage(2);
+                    pagination.changePageSize(options.pageSizes[1]);
+                    expect(pagination.getPage()).to.eventually.equal(1);
+                    pagination.changePageSize(options.pageSizes[0]);
+                });
+            }
+        }
+
+        if (options.totalItems) {
+            it('should list have the correct string for the shown items', () => {
+                let shownItems = `Showing 1-${options.defaultPageSize} of ${options.totalItems} items`;
+                expect(pagination.shownItems.getText()).to.eventually.equal(shownItems);
+            });
+
+            // execute only if the greatest items per page setting can contain all items
+            if (options.totalItems < _.last(options.pageSizes)) {
+                it('should not fail to match the upper bounds of the shown items even if not displayed', () => {
+                    let shownItems = `Showing ${options.totalItems} items`;
+                    pagination.changePageSize(_.last(options.pageSizes));
+                    expect(pagination.shownItems.getText()).to.eventually.equal(shownItems);
+                });
+            }
+        }
+    };
+};
---
+++ utils/rx-page-objects/src/rxPaginate.page.ts
@@ -0,0 +1,222 @@
+'use strict';
+
+import * as _ from 'lodash';
+import {by, ElementFinder} from 'protractor';
+import {Promise, rxComponentElement} from './rxComponent';
+import {rxMisc} from './rxMisc.page';
+
+/**
+ * @class
+ */
+export class rxPaginate extends rxComponentElement {
+
+    get lnkCurrentPage() {
+        return this.$('.pagination .active a');
+    }
+
+    get lnkFirst() {
+        return this.$('.pagination-first a');
+    }
+
+    get lnkNext() {
+        return this.$('.pagination-next a');
+    }
+
+    get lnkPrev() {
+        return this.$('.pagination-prev a');
+    }
+
+    get lnkLast() {
+        return this.$('.pagination-last a');
+    }
+
+    get pages() {
+        return this.$$('.pagination .pagination-page a');
+    }
+
+    get pageSize() {
+        return this.$('.pagination-per-page button[disabled="disabled"]');
+    }
+
+    get pageSizes() {
+        return this.all(by.repeater('i in pageTracking.itemSizeList'));
+    }
+
+    /**
+     * @description The ElementFinder for the "Showing x-y of z items" text.
+     * @example
+     * it('should always show that there are 50 states', () => {
+     *     var pagination = encore.rxPaginate.initialize();
+     *     expect(pagination.shownItems.getText()).to.eventually.eql('Showing 1-20 of 50 items');
+     *     pagination.changePageSize(50);
+     *     expect(pagination.shownItems.getText()).to.eventually.eql('Showing 50 items');
+     * });
+     */
+    get shownItems() {
+        return this.element(by.binding('pageTracking'));
+    }
+
+    getPage() {
+        return this.lnkCurrentPage.getText().then(numString => parseInt(numString, 10));
+    }
+
+    /**
+     * @description The tables that rxPaginate operates on tend to be squirly, and elements change position
+     * frequently, especially when the number of items in the table changes due to page size.  This protected
+     * utility function will help ensure elements are clickable before clicking them.
+     */
+    protected scrollAndClickIfDisplayed(elem: ElementFinder): Promise<void> {
+        return elem.isDisplayed().then(displayed => {
+            if (displayed) {
+                rxMisc.scrollToElement(elem, {positionOnScreen: 'middle'});
+                return elem.click();
+            }
+        });
+    }
+
+    /**
+     * @description Jump to a specific page, navigating backwards or forwards if necessary.
+     * @example
+     * it('should navigate to the tenth page', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     pagination.jumpToPage(10);
+     *     expect(pagination.getPage()).to.eventually.eql(10);
+     * });
+     */
+    jumpToPage(page: number): Promise<void> {
+        return this.getPages().then(pages => {
+            let index = pages.indexOf(page);
+            // If not found, we need to either click the min or max available page.
+            if (index === -1) {
+                // If the page is less than min visible pages, we need to click the first link,
+                // otherwise we should click the last.
+                index = _.min(pages) > page ? 0 : pages.length - 1;
+                this.scrollAndClickIfDisplayed(this.pages.get(index));
+                // Then call this function recursively.
+                return this.jumpToPage(page);
+            }
+            this.scrollAndClickIfDisplayed(this.pages.get(index));
+        });
+    }
+
+    /**
+     * @description Visit the first page in the paginated table by clicking the "First" link,
+     * unless the user is already on the first page. In that case, do nothing.
+     * @example
+     * it('should put the newly created resource at the top of the list', () => {
+     *     new rxPaginate($('rx-pagination')).first();
+     *     expect(myPage.someTable.row(0).name).to.eventually.contain('Created by Automated Test');
+     * });
+     */
+    first() {
+        return this.scrollAndClickIfDisplayed(this.lnkFirst);
+    }
+
+    /**
+     * @description Go back one page from the current position in the table.
+     * @example
+     * it('should go back a page', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     expect(pagination.getPage()).to.eventually.equal(3);
+     *     pagination.previous();
+     *     expect(pagination.getPage()).to.eventually.equal(2);
+     * });
+     */
+    previous() {
+        return this.scrollAndClickIfDisplayed(this.lnkPrev);
+    }
+
+    /**
+     * @description Go forward one page from the current position in the table.
+     * @example
+     * it('should go forward a page', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     expect(pagination.getPage()).to.eventually.equal(1);
+     *     pagination.next();
+     *     expect(pagination.getPage()).to.eventually.equal(2);
+     * });
+     */
+    next() {
+        return this.scrollAndClickIfDisplayed(this.lnkNext);
+    }
+
+    /**
+     * @description Visit the last page in the paginated table by clicking the "Last" link,
+     * unless the user is already on the last page. In that case, do nothing.
+     * @example
+     * it('should put the newly created resource at the bottom of the list', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     pagination.last();
+     *     expect(myPage.someTable.row(-1).name).to.eventually.contain('Created by Automated Test');
+     * });
+     */
+    last() {
+        return this.scrollAndClickIfDisplayed(this.lnkLast);
+    }
+
+    /**
+     * @description A list of all available page numbers that can be
+     * paginated to directly. Click the last page number in this list
+     * to cycle through to the next group of pages, and continue forward
+     * to the desired page number. Internal function that supports {@link rxPaginate#jumpToPage}.-*;p
+     * @example
+     * it('should support jumping to page five directly, without cycling', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     expect(encore.rxPaginate.initialize().getPages()).to.eventually.equal([1, 2, 3, 4, 5]);
+     * });
+     */
+    getPages() {
+        return this.pages.getText().then(pageNumbers => {
+            return _.map(pageNumbers, pageNumber => parseInt(pageNumber, 10));
+        });
+    }
+
+    /**
+     * @description A list of available page sizes that can be queried and adjusted
+     * by using {@link rxPaginate#pageSize}.
+     * @example
+     * it('should show all 50 states without paginating', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     expect(pagination.getPageSizes()).to.eventually.equal([20, 50, 100, 200]);
+     * });
+     */
+    getPageSizes() {
+        return this.pageSizes.getText().then(pageSizes => {
+            return _.map(pageSizes, pageSize => parseInt(pageSize, 10));
+        });
+    }
+
+    /**
+     * @description The current number of visible items on a single page in the table.
+     * @example
+     * it('should change the page size to make it easier to find things', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     expect(pagination.getPageSize()).to.eventually.equal(20);
+     * });
+     * @see {@link rxPaginate#changePageSize}
+     */
+    getPageSize() {
+        return this.pageSize.getText().then(parseInt);
+    }
+
+    /**
+     * @description Change the number of visible items on a single page in the table.
+     * @example
+     * it('should change the page size to make it easier to find things', () => {
+     *     let pagination = new rxPaginate($('rx-pagination'));
+     *     expect(pagination.getPageSize()).to.eventually.equal(20);
+     *     expect(statesTable.count()).to.eventually.equal(20);
+     *     pagination.changePageSize(200);
+     *     expect(pagination.getPageSize()).to.eventually.equal(200);
+     *     expect(statesTable.count()).to.eventually.equal(118);
+     *     // you can now search the table for all values without paginating
+     * });
+     * @see {@link rxPaginate#getPageSize}
+     */
+    changePageSize(pageSize: number) {
+        let lnkPage = this.pageSizes.filter(elem => {
+            return elem.getText().then(text => (parseInt(text, 10) === pageSize));
+        }).first();
+        return this.scrollAndClickIfDisplayed(lnkPage);
+    }
+}
