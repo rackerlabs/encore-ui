9915f0828869ee4c66227f50721777671365c5e9
jj4th <john.andrichak@gmail.com>
2017-03-15 16:05:53 -0500

    chore(rxPageObjects): rxMisc to TypeScript

--- utils/rx-page-objects/src/rxMisc.page.ts
+++ utils/rx-page-objects/src/rxMisc.page.ts
@@ -1,19 +1,30 @@
-var _ = require('lodash');
-var ElementFinder = require('protractor/built/element').ElementFinder;
-var ElementArrayFinder = require('protractor/built/element').ElementArrayFinder;
+'use strict';
+
+import * as _ from 'lodash';
+import {browser, ElementArrayFinder, ElementFinder, promise} from 'protractor';
+import {ILocation} from 'selenium-webdriver';
+import {Promise} from './rxComponent';
 
 /**
- * @namespace
+ * @interface
  */
-exports.rxMisc = {
+interface IScrollToElementOptions {
+    elementTargetPoint?: string;
+    positionOnScreen?: string;
+}
+
+type rxMiscLocationSubject = ElementFinder | ElementArrayFinder | ILocation;
+
+/**
+ * @class
+ */
+export namespace rxMisc {
     /**
-     * @function
      * @description Equivalent to `browser.actions().mouseDown(elem).mouseUp().perform();`.
      * This function should be used when dealing with odd or unusual behavior while interacting with click events
      * that don't seem to work right. Either the element does not appear to respond to a normal `.click()` call, or
      * the element is responding to more than one click event. This typically happens more often in Firefox than
      * in other browsers.
-     * @param {ElementFinder} elem - Element to "slow click".
      * @example
      * it('should click the crazy custom HTML thing that looks like a button but isn\'t', function () {
      *     var crazyButton = $('.button-wrapper[id="userId_"' + browser.params.userId + '"]');
@@ -23,31 +34,14 @@ exports.rxMisc = {
      *     expect(encore.rxNotify.all.isPresent('You will be redirected', 'success')).to.eventually.be.true;
      * });
      */
-    slowClick: function (elem) {
+    export function slowClick(elem: ElementFinder): void {
         browser.actions().mouseDown(elem).mouseUp().perform();
-    },
+    }
 
     /**
      * @description Accepts an ElementFinder, or an ElementArrayFinder, which can have several locations.
      * Should the list of elements be stacked vertically (say, in a list of table rows),
      * the element with the smallest Y coordinate will be scrolled to.
-     * @function
-     * @param {ElementFinder|ElementArrayFinder} elem - An element, or a list of elements to scroll to.
-     * @param {Object} [options={}] - The options for scrolling to the element.
-     * @param {String} [options.elementTargetPoint='top'] -
-     * Which point in the element to position when scrolling to it.
-     *
-     * - **top** : targets the top of the element
-     * - **middle** : targets the middle of the element
-     * - **bottom** : targets the bottom of the element
-     *
-     * @param {String} [options.positionOnScreen='top'] -
-     * Where to place the point in the element on the screen when scrolling to it.
-     *
-     * - **top**: position the element target point at the top of the screen
-     * - **middle** : position the element target point in the middle of the screen
-     * - **bottom** : position the element target point at the bottom of the screen
-     *
      * @example
      * var tablePage = {
      *     get tblRows() {
@@ -71,7 +65,7 @@ exports.rxMisc = {
      *    browser.ignoreSynchronization = false;
      * });
      */
-    scrollToElement: function (elem, options) {
+    export function scrollToElement(elem: ElementFinder | ElementArrayFinder, options?: IScrollToElementOptions) {
         if (options === undefined) {
             options = {};
         }
@@ -81,64 +75,63 @@ exports.rxMisc = {
             positionOnScreen: 'top', // 'middle', 'bottom'
         });
 
-        return protractor.promise.all([elem.getSize(), elem.getLocation()]).then(function (info) {
-            var size = info[0];
-            var loc = info[1];
-
-            if (_.isArray(loc)) {
-                loc = _.minBy(loc, 'y');
-            }
+        return promise.all<any>([elem.getSize(), elem.getLocation()]).then(info => {
+            // Using 'any' type because TypeScript doesn't understand that info[0] and info[1] might be arrays.
+            let size: any = info[0];
+            let loc: any = info[1];
 
             if (_.isArray(size)) {
                 size = _.minBy(size, 'height');
             }
 
-            return browser.executeScript('return window.innerHeight;').then(function (height) {
-                var positionOnScreen = {
+            if (_.isArray(loc)) {
+                loc = _.minBy(loc, 'y');
+            }
+
+            return browser.executeScript('return window.innerHeight;').then(height => {
+                let positionOnScreen = {
                     top: 0,
                     middle: height / 2,
-                    bottom: height
+                    bottom: height,
                 }[options.positionOnScreen];
 
-                var elementTargetPoint = {
+                let elementTargetPoint = {
                     top: loc.y,
                     middle: loc.y + size.height / 2,
-                    bottom: loc.y + size.height
+                    bottom: loc.y + size.height,
                 }[options.elementTargetPoint];
 
-                var yLocation = elementTargetPoint - positionOnScreen;
+                let yLocation = elementTargetPoint - positionOnScreen;
 
-                var command = ['window.scrollTo(0, ', yLocation.toString(), ');'].join('');
+                let command = ['window.scrollTo(0, ', yLocation.toString(), ');'].join('');
                 browser.executeScript(command);
             });
         });
-    },
+    }
 
     /**
-     * @function
-     * @private
      * @description
      * Unify input from either a location object or an ElementFinder into a promise
      * representing the location attribute (x or y) of either input.
      * Both `transformLocation($('.element'), 'y')` and `transformLocation({x: 20, y: 0}, 'y')`
      * return a promise representing the y value of the resulting (or provided) location object.
      */
-    transformLocation: function (elementOrLocation, attribute) {
+    export function transformLocation(elementOrLocation: rxMiscLocationSubject, attribute: string): Promise<any> {
         if (elementOrLocation instanceof ElementArrayFinder) {
             elementOrLocation = elementOrLocation.first();
         }
 
         if (elementOrLocation instanceof ElementFinder) {
-            var elem = elementOrLocation;
-            return elem.getLocation().then(function (loc) {
+            let elem = elementOrLocation;
+            return elem.getLocation().then(loc => {
                 return loc[attribute];
             });
         } else {
-            var location = elementOrLocation;
+            let location = <ILocation> elementOrLocation;
             if (_.has(location, attribute)) {
-                return protractor.promise.fulfilled(location[attribute]);
+                return promise.fulfilled(location[attribute]);
             } else {
-                return protractor.promise.fulfilled(location);
+                return promise.fulfilled(location);
             }
         }
     }
