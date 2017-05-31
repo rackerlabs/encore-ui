c575bb062675f89265b737ded703cffe816b852b
keithcruz <keith.cruz@rackspace.com>
2017-05-31 13:05:41 -0500

    test(modal): add unit tests for multimap and stackedmap

---
+++ src/utilities/rxModal/scripts/rxMultiMap.spec.js
@@ -0,0 +1,59 @@
+describe('multi map', function() {
+    var multiMap;
+
+    beforeEach(function() {
+        module('encore.ui.utilities');
+        inject(function(rxMultiMap) {
+            multiMap = rxMultiMap.createNew();
+        });
+    });
+
+    it('should add and remove objects by key', function() {
+        multiMap.put('foo', 'bar');
+
+        expect(multiMap.get('foo')).to.eql(['bar']);
+
+        multiMap.put('foo', 'baz');
+
+        expect(multiMap.get('foo')).to.eql(['bar', 'baz']);
+
+        multiMap.remove('foo', 'bar');
+
+        expect(multiMap.get('foo')).to.eql(['baz']);
+
+        multiMap.remove('foo', 'baz');
+
+        expect(multiMap.hasKey('foo')).to.be.false;
+    });
+
+    it('should support getting the keys', function() {
+        multiMap.put('foo', 'bar');
+        multiMap.put('baz', 'boo');
+
+        expect(multiMap.keys()).to.eql(['foo', 'baz']);
+    });
+
+    it('should return all entries', function() {
+        multiMap.put('foo', 'bar');
+        multiMap.put('foo', 'bar2');
+        multiMap.put('baz', 'boo');
+
+        expect(multiMap.entries()).to.eql([{
+                key: 'foo',
+                value: ['bar', 'bar2']
+            },
+            {
+                key: 'baz',
+                value: ['boo']
+            }
+        ]);
+    });
+
+    it('should preserve semantic of an empty key', function() {
+        expect(multiMap.get('key')).to.be.undefined;
+    });
+
+    it('should respect removal of non-existing elements', function() {
+        expect(multiMap.remove('foo', 'bar')).to.be.undefined;
+    });
+});
---
+++ src/utilities/rxModal/scripts/rxStackedMap.spec.js
@@ -0,0 +1,54 @@
+describe('stacked map', function() {
+    var stackedMap;
+
+    beforeEach(function () {
+        module('encore.ui.utilities');
+        inject(function (rxStackedMap) {
+            stackedMap = rxStackedMap.createNew();
+        });
+    });
+
+    it('should add and remove objects by key', function() {
+        stackedMap.add('foo', 'foo_value');
+        expect(stackedMap.length()).to.eq(1);
+        expect(stackedMap.get('foo').key).to.eq('foo');
+        expect(stackedMap.get('foo').value).to.eq('foo_value');
+
+        stackedMap.remove('foo');
+        expect(stackedMap.length()).to.eq(0);
+        expect(stackedMap.get('foo')).to.be.undefined;
+    });
+
+    it('should support listing keys', function() {
+        stackedMap.add('foo', 'foo_value');
+        stackedMap.add('bar', 'bar_value');
+
+        expect(stackedMap.keys()).to.eql(['foo', 'bar']);
+    });
+
+    it('should get topmost element', function() {
+        stackedMap.add('foo', 'foo_value');
+        stackedMap.add('bar', 'bar_value');
+        expect(stackedMap.length()).to.eq(2);
+
+        expect(stackedMap.top().key).to.eq('bar');
+        expect(stackedMap.length()).to.eq(2);
+    });
+
+    it('should remove topmost element', function() {
+        stackedMap.add('foo', 'foo_value');
+        stackedMap.add('bar', 'bar_value');
+
+        expect(stackedMap.removeTop().key).to.eq('bar');
+        expect(stackedMap.removeTop().key).to.eq('foo');
+    });
+
+    it('should preserve semantic of an empty stackedMap', function() {
+        expect(stackedMap.length()).to.eq(0);
+        expect(stackedMap.top()).to.be.undefined;
+    });
+
+    it('should ignore removal of non-existing elements', function() {
+        expect(stackedMap.remove('non-existing')).to.be.undefined;
+    });
+});
