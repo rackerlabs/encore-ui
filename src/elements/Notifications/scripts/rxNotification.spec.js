1276f5b630336a6edfd3bf5564fb98a978414d0e
Cathy Siller <cathysiller503@gmail.com>
2017-06-22 12:50:20 -0500

    refactor(Notifications): import notifications into next branch

--- src/elements/Notifications/scripts/rxNotification.spec.js
+++ src/elements/Notifications/scripts/rxNotification.spec.js
@@ -1,7 +1,8 @@
 describe('rxNotification', function () {
-    var scope, compile, el2, notifySvc;
+    var scope, compile, el, notifySvc;
     var messageText1 = 'My Message 1';
     var defaultStack = 'page';
+    var template;
 
     beforeEach(function () {
         module('encore.ui.utilities');
@@ -17,19 +18,19 @@ describe('rxNotification', function () {
 
     describe('element: Notification', function () {
         beforeEach(function () {
-            var rxNotificationTemplate = '<rx-notification type="warning">' + messageText1 + '</rx-notification>';
-            el2 = helpers.createDirective(rxNotificationTemplate, compile, scope);
+            template = '<rx-notification type="warning">' + messageText1 + '</rx-notification>';
+            el = helpers.createDirective(template, compile, scope);
         });
 
         it('should have warning CSS class when setting type attribute to warning', function () {
-            // Find the second div and check for class. Why won't it take div.rx-notification?
-            var newEl = el2.find('div').eq(1);
-            expect(newEl.hasClass('notification-warning')).to.be.true;
+            // Find the first div and check for class.
+            var wrapper = el.find('.rx-notification').eq(0);
+            expect(wrapper.hasClass('notification-warning')).to.be.true;
         });
 
         it('should contain the message', function () {
-            var newEl = el2.find('span').eq(1);
-            expect(newEl.text()).to.contain(messageText1);
+            var elText = el.find('.notification-text').eq(0);
+            expect(elText.text()).to.contain(messageText1);
         });
 
         it('should add notification to stack and remove original element', function () {
@@ -38,15 +39,15 @@ describe('rxNotification', function () {
 
             // Before compiling this notification, stack should be empty
             expect(notifySvc.stacks[defaultStack].length).to.equal(0);
-            el2 = helpers.createDirective(stackTemplate, compile, scope);
+            el = helpers.createDirective(stackTemplate, compile, scope);
 
             // Stack should now have the contents of the notification directive
             expect(notifySvc.stacks[defaultStack].length).to.equal(1);
             expect(notifySvc.stacks[defaultStack][0].type).to.equal('info');
             expect(notifySvc.stacks[defaultStack][0].text).to.contain(messageText1);
 
-            // el2 is now <div></div> and it should have no children
-            expect(el2.children().length).to.equal(0);
+            // el is now <div></div> and it should have no children
+            expect(el.children().length).to.equal(0);
         });
     });
 });
